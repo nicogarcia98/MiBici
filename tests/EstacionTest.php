9446f779150fac9c2180d5dffcab988eac6d6697
Lautaro Martin <31867574+Albertonisman@users.noreply.github.com>
2017-09-19 17:35:36 -0300

    Creamos test para anclaje y constructor

--- tests/EstacionTest.php
+++ tests/EstacionTest.php
@@ -8,7 +8,10 @@ class EstacionTest extends TestCase {
 
     public function testEstacionEnServicio() {
         // Las estaciones nuevas siempre están en servicio.
-        $estacion = new Estacion;
+        // Primero en el construct es anclajes disponibles y el segundo es anclajes totales
+        $estacion = new Estacion(10,18);
+        $this->assertEqual($estacion->anclajeDisponibles(),10);
+        $this->assertEqual($estacion->anclajeTotales(),18);
         $this->assertTrue($estacion->enServicio());
     }
 
