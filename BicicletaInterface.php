6026e2dec1921da43c0e67b9d4a773d3535f8cf7
Kkere <marcosenjuto3@gmail.com>
2017-10-24 17:26:35 -0300

    Eliminados archivos duplicados

--- BicicletaInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Mibici;
-
-interface BicicletaInterface {
-
-    public function patente();
-
-}
--- PasajeroInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Mibici;
-
-interface PasajeroInterface {
-
-    public function dni();
-
-}
