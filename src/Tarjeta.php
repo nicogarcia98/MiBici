0175d41fb1498c92b165011d29ba495c09723b08
Kkere <marcosenjuto3@gmail.com>
2017-10-24 18:00:48 -0300

    Trabajando en tarjeta.php

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -7,9 +7,9 @@ class Tarjeta {
 	private $lastime=0;
 	private $tras1=0;
 	private $trasbordo=0;
+	private $tarifa=9,7;
 
- public function pagar(Transporte $transporte, $fecha_y_hora){
-	if($transporte->tipo=='Colectivo'){
+ public function pagarbus($fecha_y_hora, $medio){
 
 		if((date('D')=='Sat' && date('h')<14 && date('h')>6) || ((date('D')!='Sun' && date('D')!='Sat') && date('h')<22 && date('h')>6))){
 			if(time()-$lastime <= 3600){
@@ -30,12 +30,21 @@ class Tarjeta {
 	}		if($this->tras1 == 1){
 			$this->trasbordo=0;		
 		}
-		
+	
+	
+		if($medio==1){
+			if($this->trasbordo==1){
+				if($this->tarifa*0.5*0.33 <= $this->saldo)					{
+					return ($this->tarifa*0.5*0.33);
+				}else{
+					return -1;
+				}
+			}
+		}
 
 
 
 
-}
  public function recargar($monto);
  public function viajesRealizados();
 
