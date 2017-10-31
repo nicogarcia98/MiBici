298dd8617deda09697a77defd42e105901249b3f
Kkere <marcosenjuto3@gmail.com>
2017-10-31 13:42:14 -0300

    Trabajando en Tarjeta.php, cambiamos la ubicacion de las interfaces temporalmente para mas comodidad en src

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -8,9 +8,17 @@ class Tarjeta {
 	private $tras1=0;
 	private $trasbordo=0;
 	private $tarifa=9,7;
+	private $plus=0;
+	private $id;
+	private $linea;
+	private $tipo;
 
- public function pagarbus($fecha_y_hora, $medio){
+public function __construct($id){
+	$this->id = $id;
+}
 
+ public function pagarbus($fecha_y_hora, $medio, $linea, $idboleto){
+ 		$time1=strtotime($fecha_y_hora);
 		if((date('D')=='Sat' && date('h')<14 && date('h')>6) || ((date('D')!='Sun' && date('D')!='Sat') && date('h')<22 && date('h')>6))){
 			if(time()-$lastime <= 3600){
 				$this->trasbordo=1;				
@@ -24,28 +32,124 @@ class Tarjeta {
 		}else{
 			$this->trasbordo=0;
 		}	
-	}
+	
 
 
-	}		if($this->tras1 == 1){
+			if($this->tras1 == 1){
 			$this->trasbordo=0;		
 		}
 	
 	
 		if($medio==1){
 			if($this->trasbordo==1){
-				if($this->tarifa*0.5*0.33 <= $this->saldo)					{
-					return ($this->tarifa*0.5*0.33);
+				if($this->tarifa*0.5*0.33 <= $this->saldo){
+					$this->saldo=$this->saldo-$this->tarifa*0.5*0.33;
+					$this->tras1=1;
+					$this->lastime=$time1;
+					return;
 				}else{
-					return -1;
+					if($this->plus<2){
+						$this->plus++;
+						echo "Plus " . $this->plus;
+						$this->tras1=0;
+						$this->lastime=$time1;
+
+						return;
+					}else{
+						echo "Saldo insuficiente";
+						return;
+					}
 				}
-			}
+				}else{
+				if($this->tarifa*0.5 <= $this->saldo){
+					$this->saldo=$this->saldo-$this->tarifa*0.5;
+					$this->lastime=$time1;
+				}else{
+					if($this->plus<2){
+						$this->lastime=$time1;
+						$this->plus++;
+						echo "Plus " . $this->plus;
+						$this->tras1=0;
+						return;
+					}else{
+						echo "Saldo insuficiente";
+						return;
+					}
+
+				}
+				}
+		}else{
+					if($this->trasbordo==1){
+				if($this->tarifa*0.33 <= $this->saldo){
+					$this->saldo=$this->saldo-$this->tarifa*0.33;
+					$this->tras1=1;
+					$this->lastime=$time1;
+
+					return;
+				}else{
+					if($this->plus<2){
+						$this->plus++;
+						echo "Plus " . $this->plus;
+						$this->lastime=$time1;
+						$this->tras1=0;
+						return;
+					}else{
+						echo "Saldo insuficiente";
+						return;
+					}
+				}
+				}else{
+				if($this->tarifa <= $this->saldo){
+					$this->saldo=$this->saldo-$this->tarifa;
+					$this->lastime=$time1;
+				}else{
+					if($this->plus<2){
+						$this->plus++;
+						echo "Plus " . $this->plus;
+						$this->tras1=0;
+						$this->lastime=$time1;
+						return;
+					}else{
+						echo "Saldo insuficiente";
+						return;
+					}
+
+				}
+				}
+
 		}
-
-
-
-
- public function recargar($monto);
- public function viajesRealizados();
-
 }
+
+
+ public function recargar($monto){
+ 	if($monto==332){
+ 		$this->saldo=$this->saldo + 388;
+ 		return;
+ 	}
+ 	if($monto==624){
+ 		$this->saldo= $this->saldo + 776;
+ 		return;
+ 	}
+ 	$this->saldo=$this->saldo + $monto;
+ 	return;
+
+ }
+ public function viajesRealizados();
+ public function get_saldo(){
+ 	return $this->saldo;
+ }
+ public function get_fecha(){
+ 	return $this->lastime;
+ }
+ public function get_tipo(){
+ 	return $this->tipo;
+ }
+ public function get_id(){
+ 	return $this->id;
+ }
+ public function get_linea(){
+ 	return $this->linea;
+ }
+}
+
+
