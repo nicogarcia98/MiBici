5de3a9a827373d2fbdf2f6a6f290d71a8221c9c3
nicogarcia98 <32110818+nicogarcia98@users.noreply.github.com>
2017-10-31 17:00:59 -0300

    Implementamos el constructor e imprimir que son llamados por tarjeta

--- src/boleto.php
+++ src/boleto.php
@@ -7,30 +7,45 @@ date_default_timezone_set('America/Argentina/Buenos_Aires');
 
 
 class boleto{
-	private $fecha=time();
-	private $tipo;
+	private $fecha;
 	private $saldo;
 	private $ID;
-	private $linea;	
-	private $tras;
-	private $monto;
-	public function trasbordo($last, $tras1){
-		if($tras1 == 1){
-			return 0;		
+	private $linea;
+	private $tarifa;
+	private $plus;
+	private $medio;
+	private $trasbordo;
+	
+	public function __construct($trasbordo, $fecha, $linea, $plus, $medio, $id, $costo, $saldo){
+		$this->trasbordo=$trasbordo;
+		$this->fecha=$fecha;
+		$this->ID=$id;
+		$this->linea=$linea;
+		$this->tarifa=$costo;
+		$this->plus=$plus;
+		$this->medio=$medio;
+		$this->trasbordo=$trasbordo;
+		$this->saldo=$saldo;
 		}
-		if((date('D')=='Sat' && date('h')<14 && date('h')>6) || ((date('D')!='Sun' && date('D')!='Sat') && date('h')<22 && date('h')>6))){
-			if(time()-$last <= 3600){
-				return 1;				
-			}else{
-				return 0;
-			}
-		}else{
-			if(time()-$last <= 5400){
-				return 1;
-			}		
-		}else{
-			return 0;
-		}	
+	public function imprimir(){
+		echo date("d-m-Y H:i:s", $fecha);
+		echo "Linea " . $this->linea;
+		echo $this->tarifa;
+		if($this->plus > 0){
+			echo "Plus " . $this->plus;
+		}
+		if($this->trasbordo==1){
+		echo "Trasbordo";
+		}
+		if($this->medio == 1){
+		echo "Medio"
+		}
+		if($this->medio == 0 && $this->trasbordo==0 && $this->plus==0){
+		echo "Normal";
+		}
+		echo $this->ID;
+		echo $this->saldo;
+		
 	}
 }
 
