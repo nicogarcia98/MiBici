59684ce099fe4d06e6292f598558d334ff0b5bc4
nicogarcia98 <32110818+nicogarcia98@users.noreply.github.com>
2017-10-31 17:50:37 -0300

    Update and rename EstacionTest.php to TarjetaTest.php

---
+++ tests/TarjetaTest.php
@@ -0,0 +1,13 @@
+
+<?php
+namespace MiBici;
+use PHPUnit\Framework\TestCase;
+class EstacionTest extends TestCase {
+    /**
+     * Comprueba que el saldo de una tarjeta nueva sea cero.
+     */
+    public function testSaldoCero() {
+        $tarjeta = new Tarjeta;
+        $this->assertEquals($tarjeta->saldo(), 0);
+    }
+}
