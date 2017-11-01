a100cb0744cb0860a010c99e22551fbe27b3f576
Kkere <31932876+Kkere@users.noreply.github.com>
2017-11-01 16:17:59 -0300

    Haciendo los primeros tests

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -7,7 +7,32 @@ class EstacionTest extends TestCase {
      * Comprueba que el saldo de una tarjeta nueva sea cero.
      */
     public function testSaldoCero() {
-        $tarjeta = new Tarjeta;
-        $this->assertEquals($tarjeta->saldo(), 0);
+        $tarjeta = new Tarjeta(1234;1)
+        $this->assertEquals($tarjeta->getSaldo(), 0);
     }
-}
+       
+    public function testSaldoMedio() {
+        $tarjeta = new Tarjeta(1234,1);
+        $this->assertEquals($tarjeta->getID(),1234);
+        $this->assertEquals($tarjeta->getTipo(),"Medio");
+    }
+    public function testSaldoCincuenta(){
+        $tarjeta = new Tarjeta(1234,0);
+        $tarjeta->recargar(50);
+        $this->assertEquals($tarjeta->getSaldo(),50);
+    }
+    public function testTrasbordo(){
+        $tarjeta = new Tarjeta(1234, 0);
+        $tarjeta->recargar(30);
+        $colectivo = new Colectivo ("133 negro");
+        $tarjeta->pagarbus("2017/09/03 14:12",0,$colectivo);
+        $this->assertEquals($tarjeta->getSaldo(),20.3);
+        $tarjeta->pagarbus("2017/09/03 14:12",0,$colectivo);
+        $this->assertEquals($tarjeta->getSaldo(),17.1);
+        
+    }
+
+    
+               
+}                               
+                               
