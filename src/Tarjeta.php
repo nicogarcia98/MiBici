f3f312caee3955a3ba1098fd22e60902231345cb
Kkere <31932876+Kkere@users.noreply.github.com>
2017-11-01 16:17:56 -0300

    Correcciones de clases

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -3,6 +3,7 @@
 namespace MiBici;
 
 class Tarjeta {
+	private $medio=0;
 	private $saldo=0;
 	private $lastime=0;
 	private $tras1=0;
@@ -11,32 +12,35 @@ class Tarjeta {
 	private $costo;
 	private $plus=0;
 	private $id;
-	private $linea;
 	private $tipo;
 	private $time1;
 	private $viajes = array();
 	private $lastbici=0;
-public function __construct($id){
+	
+public function __construct($id, $medio){
 	$this->id = $id;
+	$this->medio=$medio;
+	
 }
 
-public function pagarbici($fecha_y_hora, $paten){
+public function pagarbici($fecha_y_hora, Bicicleta $paten){
 	$this->time1=strtotime($fecha_y_hora);
 	if($this->lastbici-$this->$time1>3600){
 		$this->costo= $this->tarifa * 1.5;
 		$this->saldo= $this->saldo - $this->costo;
 		$this->lastbici=$this->time1;
-		$boleto= new boleto(0, $fecha_y_hora, $paten,0,0, $this->id,$this->costo, $this->saldo, 0);
+		$boleto= new boleto(0, $fecha_y_hora, $paten->get_pat(),0,0, $this->id,$this->costo, $this->saldo, 0);
 		array_push($viajes, $boleto);
 		return;
 	}else{
-		$boleto= new boleto(0, $fecha_y_hora, $paten,0,0, $this->id,$this->costo, $this->saldo, 0);
+		$boleto= new boleto(0, $fecha_y_hora, $paten->get_pat(),0,0, $this->id,$this->costo, $this->saldo, 0);
 		array_push($viajes, $boleto);
+		return;
 	}
 	
 	
 }
- public function pagarbus($fecha_y_hora, $medio, $linea){
+ public function pagarbus($fecha_y_hora, $medio, Colectivo $linea){
  		$this->time1=strtotime($fecha_y_hora);
 		if((date('D', $this->time1)=='Sat' && date('h', $this->time1)<=14 && date('h', $this->time1)>=6) || ((date('D', $this->time1)!='Sun' && date('D', $this->time1)!='Sat') && date('h', $this->time1)<=22 && date('h', $this->time1)>=6)){
 			if($this->time1-$this->lastime <= 3600){
@@ -60,18 +64,18 @@ public function pagarbici($fecha_y_hora, $paten){
 		}
 
 		$this->costo=$this->tarifa;
-		if($medio==1){
-			$this->costo=$this->costo* 0.5;
+		if($medio==1 && $this->medio==1){
+			$this->costo=round($this->costo* 0.5,2);
 		}
 		if($this->trasbordo==1 && $this->tras1 == 0){
-			$this->costo=$this->costo* 0.33;
+			$this->costo=round($this->costo* 0.33,2);
 			$this->tras1 = 1;
 		}else{
 			$this->tras1=0;
 		}
 
 		if($this->plus > 0 && $this->saldo >= $this->costo + $this->tarifa * $this->plus){
-			$this->costo=$this->costo + $this->tarifa * $this->plus;
+			$this->costo=round($this->costo + $this->tarifa * $this->plus);
 			$this->plus=0;
 		}else{
 			if($this->plus==1){
@@ -81,9 +85,9 @@ public function pagarbici($fecha_y_hora, $paten){
 			}
 		}
 			if($this->costo <= $this->saldo ){
-				$this->saldo=$this->saldo-$this->costo;
+				$this->saldo=round($this->saldo-$this->costo,2);
 				$this->lastime=$time1;
-				$boleto= new boleto($this->tras1, $this->lastime, $this->linea, $this->plus, $medio, $this->id,$this->costo, $this->saldo, 1);
+				$boleto= new boleto($this->tras1, $this->lastime, $linea->get_linea(), $this->plus, $medio, $this->id,$this->costo, $this->saldo, 1);
 				array_push($viajes, $boleto);
 				$boleto->imprimir();
 				return;
@@ -92,7 +96,7 @@ public function pagarbici($fecha_y_hora, $paten){
 						$this->plus++;
 						$this->tras1=0;
 						$this->lastime=$time1;
-						$boleto= new boleto($this->tras1, $this->lastime, $this->linea, $this->plus, $medio, $this->id, 0, $this->saldo, 1);
+						$boleto= new boleto($this->tras1, $this->lastime, $linea->get_linea(), $this->plus, $medio, $this->id, 0, $this->saldo, 1);
 						array_push($viajes, $boleto);
 						$boleto->imprimir();
 						return;
@@ -102,8 +106,27 @@ public function pagarbici($fecha_y_hora, $paten){
 						}
 			}	
 	}
+public function getSaldo(){
+	return $this->saldo;
+}
+	
+public function getCosto(){
+	return $this->costo;
+}
 
- 
+public function getTipo(){
+	if($this->medio == 1 && $medio == 1){
+		return "Medio";
+	}
+	return "Normal";
+}
+	
+public function getID(){
+	return $this->id;	
+}
+public function getFecha(){
+	return date("d-m-Y H:i:s",$this->lastime);
+}
  public function recargar($monto){
  	if($monto==332){
  		$this->saldo=$this->saldo + 388;
@@ -121,7 +144,7 @@ public function viajesRealizados(){
 	for($i=0;$i<count($viajes);$i++){
 		$viajes[$i]->imprimir();
 	}
-	}
+}
 }
 
 
