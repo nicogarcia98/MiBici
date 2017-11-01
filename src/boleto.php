92b7c4299bb4f6809133f80b987c6680d92cf0a2
Kkere <31932876+Kkere@users.noreply.github.com>
2017-11-01 15:22:41 -0300

    Update boleto.php

--- src/boleto.php
+++ src/boleto.php
@@ -10,41 +10,52 @@ class boleto{
 	private $fecha;
 	private $saldo;
 	private $ID;
-	private $linea;
+	private $lineaopat;
 	private $tarifa;
 	private $plus;
 	private $medio;
 	private $trasbordo;
-	
-	public function __construct($trasbordo, $fecha, $linea, $plus, $medio, $id, $costo, $saldo){
+	private $bob=1;
+	public function __construct($trasbordo, $fecha, $lineaopat, $plus, $medio, $id, $costo, $saldo, $bob){
 		$this->trasbordo=$trasbordo;
 		$this->fecha=$fecha;
 		$this->ID=$id;
-		$this->linea=$linea;
+		$this->lineaopat=$lineaopat;
 		$this->tarifa=$costo;
 		$this->plus=$plus;
 		$this->medio=$medio;
 		$this->trasbordo=$trasbordo;
 		$this->saldo=$saldo;
+		$this->bob=$bob;
 		}
 	public function imprimir(){
-		echo date("d-m-Y H:i:s", $fecha);
-		echo "Linea " . $this->linea;
-		echo $this->tarifa;
-		if($this->plus > 0){
-			echo "Plus " . $this->plus;
+		if($this->bob==1){
+			echo date("d-m-Y H:i:s", $fecha);
+			echo "Linea " . $this->linea;
+			echo $this->tarifa;
+			if($this->plus > 0){
+				echo "Plus " . $this->plus;
+			}
+			if($this->trasbordo==1){
+			echo "Trasbordo";
+			}
+			if($this->medio == 1){
+			echo "Medio";
+			}
+			if($this->medio == 0 && $this->trasbordo==0 && $this->plus==0){
+			echo "Normal";
+			}
+			echo $this->ID;
+			echo $this->saldo;
+		}else{
+			echo "Viaje en la bici ";
+			echo "Patente " . $this->lineaopat;
+			echo "el dia ";
+			echo date("d-m-Y H:i:s", $fecha);
+			echo $this->tarifa;
+			echo $this->ID;
+			echo $this->saldo;
 		}
-		if($this->trasbordo==1){
-		echo "Trasbordo";
-		}
-		if($this->medio == 1){
-		echo "Medio";
-		}
-		if($this->medio == 0 && $this->trasbordo==0 && $this->plus==0){
-		echo "Normal";
-		}
-		echo $this->ID;
-		echo $this->saldo;
 		
 		
 	}
