1f0e3cd5014d78eafc3b8ff315500f6543a76e4a
Kkere <31932876+Kkere@users.noreply.github.com>
2017-11-02 17:07:06 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -22,15 +22,38 @@ class EstacionTest extends TestCase {
         $tarjeta = new Tarjeta(1234,0);
         $tarjeta->recargar(50);
         $this->assertEquals($tarjeta->getSaldo(),50);
+    
     }
+    
+    public function testSaldoPromo(){
+        $tarjeta = new Tarjeta(1234,0);
+        $tarjeta->recargar(332);
+        $this->assertEquals($tarjeta->getSaldo(),388);
+        $tarjeta1 = new Tarjeta(1235,0);
+        $tarjeta1->recargar(624);
+        $this->assertEquals($tarjeta1->getSaldo(),776);
+        
+    }
+    
+    public function testTrasbordoyMedio(){
+        $tarjeta = new Tarjeta(1234, 1);
+        $tarjeta->recargar(50);
+        $colectivo = new Colectivo ("133 negro");
+        $tarjeta->pagarbus("2017/09/04 13:40",0,$colectivo);
+        $this->assertEquals($tarjeta->getSaldo(),45.15);
+        $tarjeta->pagarbus("2017/09/04 14:12",0,$colectivo);
+        $this->assertEquals($tarjeta->getSaldo(),43.55);
+        
+    }
+    
     public function testTrasbordo(){
         $tarjeta = new Tarjeta(1234, 0);
         $tarjeta->recargar(50);
         $colectivo = new Colectivo ("133 negro");
-        $tarjeta->pagarbus("2017/09/03 13:40",0,$colectivo);
-        $this->assertEquals($tarjeta->getSaldo(),50);
+        $tarjeta->pagarbus("2017/09/03 13:02",0,$colectivo);
+        $this->assertEquals($tarjeta->getSaldo(),40.3);
         $tarjeta->pagarbus("2017/09/03 14:12",0,$colectivo);
-        $this->assertEquals($tarjeta->getSaldo(),17.1);
+        $this->assertEquals($tarjeta->getSaldo(),37.1);
         
     }
 
