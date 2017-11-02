db1bc15709c68a8ca81b5f884219d05a9e3f7cf4
Kkere <31932876+Kkere@users.noreply.github.com>
2017-11-02 16:51:06 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -27,7 +27,7 @@ class EstacionTest extends TestCase {
         $tarjeta = new Tarjeta(1234, 0);
         $tarjeta->recargar(50);
         $colectivo = new Colectivo ("133 negro");
-        $tarjeta->pagarbus(time(),0,$colectivo);
+        $tarjeta->pagarbus("2017/09/03 13:40",0,$colectivo);
         $this->assertEquals($tarjeta->getSaldo(),50);
         $tarjeta->pagarbus("2017/09/03 14:12",0,$colectivo);
         $this->assertEquals($tarjeta->getSaldo(),17.1);
