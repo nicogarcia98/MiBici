f74c6ed80829fbcbacb6a49e1937696cc240632e
nicogarcia98 <32110818+nicogarcia98@users.noreply.github.com>
2017-11-03 16:39:46 -0300

    Update Colectivo.php

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -1,12 +1,12 @@
 <?php
+
 namespace MiBici;
+
 class Colectivo {
     protected $num;
-    
     public function get_linea() {
         return $this->num;
     }
-    
     public function __construct ($linea) {
         $this->num = $linea;
     }
