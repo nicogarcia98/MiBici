ef5ff0ef57b6b93d9295108a2a52339ab937566f
nicogarcia98 <32110818+nicogarcia98@users.noreply.github.com>
2017-11-03 16:18:16 -0300

    Estandar

--- src/Bicicleta.php
+++ src/Bicicleta.php
@@ -1,13 +1,12 @@
 <?php
 
 namespace MiBici;
+
 class Bicicleta {
     private $pat;
-    
     public function get_pat() {
         return $this->pat;
     }
-
     public function __construct ($patente) {
         $this->pat = $patente;
     }
